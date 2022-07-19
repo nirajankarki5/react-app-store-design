636a33e2dd1ddd6c50c591e1530fe04835c41b23
nirajankarki5 <nirajankarki5@gmail.com>
2022-07-19 14:15:35 +0545

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
+import { IoNotifications, IoSearchOutline } from "react-icons/io5";
+
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className={styles.navbar}>
       <h1 className={styles.heading}>Overview</h1>
+      <div className={styles["nav-actions"]}>
+        <button className={styles["icon-btn"]}>
+          <IoNotifications className={styles.icon} />
+        </button>
+        <input type="text" placeholder="Search" className={styles.input} />
+        <IoSearchOutline className={styles.search} />
+
+        <img src="/images/user.jpg" alt="user" className={styles.user} />
+      </div>
     </nav>
   );
 };

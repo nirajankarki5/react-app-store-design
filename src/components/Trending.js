e8fed694f4d8eed69cbf20f469a687a23c8fa69d
nirajankarki5 <nirajankarki5@gmail.com>
2022-07-21 14:36:09 +0545

    added trending

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,5 +1,49 @@
+import { trendingList } from "../data/trending-list";
+import { Link } from "react-router-dom";
+import styles from "./trending.module.css";
+
 const Trending = () => {
-  return <section className="trending">Trending</section>;
+  return (
+    <aside className={styles.trending}>
+      <div className={styles["trending-container"]}>
+        <header className={styles.heading}>
+          <h3>Trending</h3>
+          <Link to="/">Show all</Link>
+        </header>
+        <ul>
+          {trendingList.map((item) => {
+            return (
+              <li key={item.id}>
+                <Link to="/" className={styles["trending-list"]}>
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className={styles.image}
+                  />
+                  <div className={styles.name}>
+                    <h5>{item.name}</h5>
+                    <p>{item.type}</p>
+                  </div>
+                  <button className={styles["download-btn"]}>Download</button>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className={styles["bring-you-home-container"]}>
+        <img
+          src="/images/bring-you-home.jpg"
+          alt="bring you home"
+          className={styles["bring-you-home-img"]}
+        />
+        <h3>Bring You Home</h3>
+        <p>Puzzle Game</p>
+        <button>$21.50</button>
+      </div>
+    </aside>
+  );
 };
 
 export default Trending;
---
+++ src/data/trending-list.js
@@ -0,0 +1,26 @@
+export const trendingList = [
+  {
+    id: 1,
+    name: "Telegram",
+    type: "Social networks",
+    image: "/images/telegram.webp",
+  },
+  {
+    id: 2,
+    name: "WhatsApp Desktop",
+    type: "Trending",
+    image: "/images/whatsapp.webp",
+  },
+  {
+    id: 3,
+    name: "Machinarium",
+    type: "Adventure game",
+    image: "/images/machinarium.jpg",
+  },
+  {
+    id: 4,
+    name: "Microsoft Word",
+    type: "Text editor",
+    image: "/images/msword.png",
+  },
+];

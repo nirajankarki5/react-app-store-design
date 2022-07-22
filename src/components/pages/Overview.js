3790a4267e10c2f70549a4db3314524b8467b107
nirajankarki5 <nirajankarki5@gmail.com>
2022-07-22 14:40:49 +0545

    added cover image in main container

--- src/components/pages/Overview.js
+++ src/components/pages/Overview.js
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import styles from "./overview.module.css";
+
 const Overview = () => {
-  return <section className="main">Overview</section>;
+  return (
+    <section className="main-container">
+      <div className={styles.cover}>
+        <div className={styles["cover-details"]}>
+          <div>
+            <Link to="/">GRAPHICS AND DESIGN</Link>
+            <h4>Vectornator: Design Software</h4>
+            <p>
+              Create designs, interfaces and beautiful illustrations with this
+              tool
+            </p>
+          </div>
+          <Link to="/" className={styles["download-btn"]}>
+            Download
+          </Link>
+        </div>
+
+        <img
+          className={styles["cover-img"]}
+          src="/images/vectornator.png"
+          alt="Vectornator: Design Software"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Overview;
---
+++ src/data/top-games-app.js
@@ -0,0 +1,37 @@
+export const topGamesApp = [
+  {
+    id: 1,
+    name: "Tayasui sketches",
+    category: "Creation",
+    image: "/images/tayasui.jpg",
+    price: "Download",
+  },
+  {
+    id: 2,
+    name: "Myst",
+    category: "Adventure game",
+    image: "/images/myst.webp",
+    price: "$12.00",
+  },
+  {
+    id: 3,
+    name: "Swift Playgrounds",
+    category: "Development",
+    image: "/images/swift.png",
+    price: "$25.00",
+  },
+  {
+    id: 4,
+    name: "Subway Surfers",
+    category: "Games",
+    image: "/images/subway.jpg",
+    price: "Download",
+  },
+  {
+    id: 5,
+    name: "Pinterest",
+    category: "Design",
+    image: "/images/pinterest.png",
+    price: "Download",
+  },
+];
